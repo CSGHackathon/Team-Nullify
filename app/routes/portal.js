b95370f570ca078492c986d8947b3271492b3b53
clevernyyyy <aschaal1263@gmail.com>
2016-02-27 10:06:40 -0600

    fixing broken portal

--- app/routes/portal.js
+++ app/routes/portal.js
@@ -4,4 +4,7 @@ export default Ember.Route.extend({
   setupController: function (controller){
     controller.set('authController', this.controllerFor('auth'));
   },
+  model: function() {
+    return this.store.find('portal');
+  }
 });
