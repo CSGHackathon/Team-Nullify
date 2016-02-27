61e4609c8a38afd3dd978983eba4213fc1ab5393
Ryan Grandgenett <rmgrandgenett@unomaha.edu>
2016-02-27 09:41:21 -0600

    Fixed admin auth like a boss

--- app/controllers/portal/admin.js
+++ app/controllers/portal/admin.js
@@ -1,4 +1,18 @@
 import Ember from 'ember';
 
 export default Ember.Controller.extend({
+  authController: undefined,
+  userName: '',
+  password: '',
+  rememberMe: false,
+  actions: {
+    submitLogin: function() {
+      console.log("admin Controller");
+      var userName = this.get('userName');
+      var password = this.get('password');
+      var rememberMe = this.get('rememberMe');
+      var authController = this.get('authController');
+      authController.adminLogin({'userName': userName, 'password': password, 'rememberMe': rememberMe});
+    },
+  }
 });
